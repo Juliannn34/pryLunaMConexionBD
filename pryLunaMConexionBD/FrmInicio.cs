b3feff93e3e52d73140bd5ed622963237206b251
Juliannn34 <julianlunamariani@gmail.com>
2025-04-09 15:24:56 -0300

    Agregar archivos de proyecto.

---
+++ pryLunaMConexionBD/FrmInicio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using pryGestionDeInventario;
+
+namespace pryLunaMConexionBD
+{
+    public partial class FrmInicio : Form
+    {
+        public FrmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clsConexionBD objConectarBD = new clsConexionBD();
+
+            objConectarBD.ConectarBD();
+        }
+    }
+}
